dbd872bd45cef422a1cba0c26f1e37d8c65e1725
Simon <simonnchong@gmail.com>
2022-08-02 17:40:34 +0800

    learned optional positional and keyword argument, label, button, entry and other GUI

--- Tkinter-exercises, unlimited arg, kwarg/label,button,entry,layout.py
+++ Tkinter-exercises, unlimited arg, kwarg/label,button,entry,layout.py
@@ -4,22 +4,34 @@ window = tk.Tk() # call the class for the window GUI
 # write `window = Tk()` if `from tkinter import Tk`
 window.title("My first GUI") # set the title of the window
 window.minsize(width = 500, height = 300) # scale itself to the content if over the size
-
+window.config(padx = 200, pady = 300) # add space around the window
 
 
 
 #? ---------------------------------------------------------------- create label ----------------------------------------------------------------
 
 my_label = tk.Label(text = "Label placeholder", font = ("Arial", 24, "bold")) # set the name will be displayed in the label, and the font properties
-my_label.pack() # place the label on screen, place top center in default
+# my_label.pack() # place the from top to bottom, logically arrange
 # pack(side = "right") # put the label at the right of screen, responsive to window size # left, top, bottom
 # pack(expand = True) # put the label at the center of screen, responsive to window size
+#! you cant usee pack() and grid() in the same file!!
 
-#! reference : http://tcl.tk/man/tcl8.6/TkCmd/pack.htm
 
 #* other than the constructor passing setting as on line 9, there are 2 more approaches to do so
 my_label["text"] = "Label placeholder" # access the dictionary 
 my_label.config(text = "Label placeholder") # access the method of setter
+
+
+# #* use place() for layout
+# my_label.place(x = 0, y = 0) # the origin is at top left corner
+
+#* use grid() for layout
+my_label.grid(row = 0, column = 0)
+
+my_label.config(padx = 20, pady = 30) # add space around the widget
+
+
+#! reference : http://tcl.tk/man/tcl8.6/TkCmd/pack.htm
 
 
 
@@ -34,7 +46,18 @@ def button_click():
     
     
 my_button = tk.Button(text = "Button Placeholder", command = button_click) # command receive the name of the function instead of the function itself, so no parenthesis needed
-my_button.pack()
+
+
+#* use grid() for layout
+my_button.grid(row = 2, column = 2)
+
+
+my_button2 = tk.Button(text = "Button Placeholder", command = button_click) # command receive the name of the function instead of the function itself, so no parenthesis needed
+
+
+#* use grid() for layout
+my_button2.grid(row = 1, column = 3)
+
 
 #! http://tcl.tk/man/tcl8.6/TkCmd/button.htm
 
@@ -47,11 +70,13 @@ my_button.pack()
 #? ---------------------------------------------------------------- create entry (input box) ----------------------------------------------------------------
 
 my_input = tk.Entry(width = 10) # set the width
-my_input.pack()
+
+
+#* use grid() for layout
+my_input.grid(row = 4, column = 4)
 
 
 #! http://tcl.tk/man/tcl8.6/TkCmd/entry.htm
-
 
 
 
